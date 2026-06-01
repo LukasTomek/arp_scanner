44d80aef29fe1baa8f9a24bbeab3904181333ebb
Lukas Tomek <l.tomek@kostal.com>
2026-06-01 13:15:39 +0200

    Repair nonexisting function.

--- components/arp_scanner/__init__.py
+++ components/arp_scanner/__init__.py
@@ -14,6 +14,5 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     
-    # Force the builder engine to compile the new source file asset
-    cg.add_library("lwip", None) # Double-checks core framework linking
-    cg.add_sources(["arp_scanner.cpp"])
+    # FIX: Correct way to register a .cpp source file asset in ESPHome
+    cg.add_build_file("arp_scanner.cpp")
